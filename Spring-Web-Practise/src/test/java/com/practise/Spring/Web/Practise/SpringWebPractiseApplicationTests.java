281c7dd54080e9d39bf5c7c830b027970feb5840
Mr1n8L <mrinaljani1822@gmail.com>
2024-10-05 01:36:41 +0530

    Initial config

---
+++ Spring-Web-Practise/src/test/java/com/practise/Spring/Web/Practise/SpringWebPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.Spring.Web.Practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

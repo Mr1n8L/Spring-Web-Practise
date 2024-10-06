d8fdf5f45db853833a2c32d28fd6305ea1199379
Mr1n8L <mrinaljani1822@gmail.com>
2024-10-07 03:30:00 +0530

    Using @RestController and @RequestMapping for URL

--- src/main/java/com/practise/Spring/Web/Practise/homeController.java
+++ src/main/java/com/practise/Spring/Web/Practise/homeController.java
@@ -1,10 +1,12 @@
 package com.practise.Spring.Web.Practise;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class homeController {
 
+    @RequestMapping("/")
     public String greet(){
         return "Hello, World!";
     }

9200fad0236189209b6cd83f829882d64298dac8
Mr1n8L <mrinaljani1822@gmail.com>
2024-10-07 17:46:22 +0530

    .

--- src/main/java/com/practise/Spring/Web/Practise/homeController.java
+++ src/main/java/com/practise/Spring/Web/Practise/homeController.java
@@ -1,5 +1,5 @@
 package com.practise.Spring.Web.Practise;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
